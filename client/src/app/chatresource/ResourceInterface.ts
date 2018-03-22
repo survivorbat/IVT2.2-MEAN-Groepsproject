e4a2d8e74aa7147d3044635690e06ead84c2c45c
survivorbat <mheijden15@avans.nl>
2018-03-22 13:16:18 +0100

    Added httpclientmodule and film service

--- client/src/app/chatresource/ResourceInterface.ts
+++ client/src/app/chatresource/ResourceInterface.ts
@@ -1,7 +1,8 @@
 import Resource from "./domain/Resource";
+import { Observable } from "rxjs/Observable";
 
 export default interface ResourceInterface {
-    searchItem(item: String): Resource;
-    getItem(item: Resource): Resource;
-    getItems(): Resource[];
+    searchItem(item: String): Observable<Resource>;
+    getItem(item: Resource): Observable<Resource>;
+    getItems(): Observable<Resource[]>;
 }
--- client/src/app/chatresource/services/film.service.ts
+++ client/src/app/chatresource/services/film.service.ts
@@ -1,19 +1,22 @@
-import { Injectable } from '@angular/core';
-import ResourceInterface from '../ResourceInterface';
-import Resource from '../domain/Resource';
+import { Injectable } from '@angular/core'
+import ResourceInterface from '../ResourceInterface'
+import Resource from '../domain/Resource'
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs/Observable'
 
+const BASE_URL = "https://avancinema.herokuapp.com/api/films"
 @Injectable()
 export class FilmService implements ResourceInterface {
 
-  searchItem(item: String): Resource {
-    throw new Error("Method not implemented.");
+  searchItem(item: String): Observable<Resource> {
+    return this.http.get<Resource>(BASE_URL)
   }
-  getItem(item: Resource): Resource {
-    throw new Error("Method not implemented.");
+  getItem(item: Resource): Observable<Resource> {
+    return this.http.get<Resource>(BASE_URL+'/'+item.id)
   }
-  getItems(): Resource[] {
-    throw new Error("Method not implemented.");
+  getItems(): Observable<Resource[]> {
+   return this.http.get<Resource[]>(BASE_URL)
   }
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
 }
