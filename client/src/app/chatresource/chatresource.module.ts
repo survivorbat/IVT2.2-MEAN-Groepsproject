7298619dc2c4051d90954c0d5d1719c5f1fbf99b
survivorbat <mheijden15@avans.nl>
2018-03-22 13:18:18 +0100

    Added declarations to chatresource

--- client/src/app/chatresource/chatresource.module.ts
+++ client/src/app/chatresource/chatresource.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { OvService } from './services/ov.service';
+import { FilmService } from './services/film.service';
+import { ChatresourceService } from './services/chatresource.service';
 
 @NgModule({
   imports: [
     CommonModule,
     HttpClientModule
   ],
-  declarations: []
+  declarations: [OvService, FilmService, ChatresourceService]
 })
 export class ChatresourceModule { }
