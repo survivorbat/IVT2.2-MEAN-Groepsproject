5a3042a7e8081244c1226ddcc86391b038a8a8c6
Gerben Droogers <gerben@droogers.net>
2018-03-24 21:56:59 +0100

    Very basic layout structure

--- client/src/app/ui/ui.module.ts
+++ client/src/app/ui/ui.module.ts
@@ -7,6 +7,7 @@ import { FooterComponent } from './components/footer/footer.component';
   imports: [
     CommonModule
   ],
-  declarations: [HeaderComponent, FooterComponent]
+  declarations: [HeaderComponent, FooterComponent],
+  exports: [HeaderComponent, FooterComponent]
 })
 export class UiModule { }
