d0f1d722f69f42d229b957e296f3c1e7d3956f24
Gerben Droogers <gerben@droogers.net>
2018-03-26 20:23:22 +0200

    Chatbox connected to server

--- client/src/app/chatbox/components/chatboxlist/chatboxlist.component.ts
+++ client/src/app/chatbox/components/chatboxlist/chatboxlist.component.ts
@@ -9,8 +9,20 @@ import Chatbox from '../../domain/Chatbox';
 })
 export class ChatboxlistComponent implements OnInit {
 
+  private chatboxes: Chatbox[];
+  private error: string;
+
   constructor(private chatboxService: ChatboxService) { }
 
   ngOnInit() {
+    this.getChatboxes();
+  }
+
+  public get Chatboxes(): Chatbox[] {
+		return this.chatboxes;
+	}
+
+  getChatboxes(): void{
+    this.chatboxService.getItems().subscribe(res => this.chatboxes=res, err => this.error="Error bij het ophalen van resources")
   }
 }
--- client/src/app/chatbox/services/chatbox.service.ts
+++ client/src/app/chatbox/services/chatbox.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
+import Chatbox from '../../domain/Chatbox';
+import { HttpClient } from '@angular/common/http';
 
+const BASE_URL = "https://groepsprojectivt2.herokuapp.com/api/chatboxes"
 @Injectable()
 export class ChatboxService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getItems(): Observable<Chatbox[]> {
+    return this.http.get<Chatbox[]>(BASE_URL)
+  }
 
 }
