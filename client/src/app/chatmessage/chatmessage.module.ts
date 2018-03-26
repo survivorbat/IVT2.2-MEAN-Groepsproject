019e839c52de065fced9eb31dd384b425596fad7
Wouter Jansen <wouter@compar.nl>
2018-03-26 21:29:09 +0100

    Commited changes for review, currently not working

--- client/src/app/chatmessage/chatmessage.module.ts
+++ client/src/app/chatmessage/chatmessage.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { MessageService } from './services/message.service';
 import { InputfieldComponent } from './components/inputfield/inputfield.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
+  ],
+  exports: [
+    InputfieldComponent  
   ],
   declarations: [InputfieldComponent],
   providers: [MessageService]
--- client/src/app/chatmessage/components/inputfield/inputfield.component.ts
+++ client/src/app/chatmessage/components/inputfield/inputfield.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+//Import various services
+import { FilmService } from '../../../chatresource/services/film.service';
+import Resource from '../../../chatresource/domain/Resource';
+
 import Message from '../../domain/Message';
 
 @Component({
@@ -7,12 +11,23 @@ import Message from '../../domain/Message';
   styleUrls: ['./inputfield.component.scss']
 })
 export class InputfieldComponent implements OnInit {
-
+  private resources: Resource[];
   private message: Message;
   
-  constructor() { }
+  constructor(private filmservice: FilmService) { }
 
   ngOnInit() {
+
+  }
+    
+  checkMessage() {
+      //check message on filmService
+    this.filmservice.getItems().subscribe(res=> {
+        console.log("cheese");
+      //this.resources=res.map(item=> {
+      //    console.log("cheese");
+      //})
+    })
   }
 
 }
