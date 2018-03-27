d2f31065dd617cef9e59671bbcd4fc11fdb67a2b
Gerben Droogers <gerben@droogers.net>
2018-03-27 17:09:30 +0200

    Login added

--- client/src/app/user/components/login/login.component.ts
+++ client/src/app/user/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from '../../services/user.service';
+import User from '../../domain/User';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public error: string;
+  public user: User = {
+    id: null,
+    username: '',
+    password: ''
+  };
+
+  constructor(private userService: UserService, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  login(): void {
+    this.userService.checkUser(this.user).subscribe(res => this.handleLog(res), err => this.error=err.error);
+  }
+
+  handleLog(answer): void {
+    window.localStorage.setItem('API_TOKEN', answer.token)
+    this.router.navigateByUrl('/m')
   }
 
 }
