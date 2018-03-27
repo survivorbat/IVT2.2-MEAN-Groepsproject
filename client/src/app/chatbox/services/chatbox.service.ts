5bf3c46dc989e074f4c85becabf8383e0e4edef6
Gerben Droogers <gerben@droogers.net>
2018-03-27 17:22:10 +0200

    HttpHeaders added at chatbox

--- client/src/app/chatbox/services/chatbox.service.ts
+++ client/src/app/chatbox/services/chatbox.service.ts
@@ -1,30 +1,35 @@
 import { Injectable } from '@angular/core';
 import Chatbox from '../domain/Chatbox';
 import { Observable } from 'rxjs/Observable';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 
+const httpOptions = {
+  headers: new HttpHeaders({ 'Authorization-Type': 'Bearer '+window.localStorage.getItem('API_TOKEN') })
+};
+
 const BASE_URL = environment.serverurl + "/api/chatboxes"
+
 @Injectable()
 export class ChatboxService {
 
   constructor(private http: HttpClient) { }
 
   getItems(): Observable<Chatbox[]> {
-    return this.http.get<Chatbox[]>(BASE_URL)
+    return this.http.get<Chatbox[]>(BASE_URL, httpOptions)
   }
   updateItem(item: Chatbox): Observable<any> {
     const url = `${BASE_URL}/${item.id}`;
-    return this.http.patch(url, item);
+    return this.http.patch(url, item, httpOptions);
   }
   addItem(item: Chatbox): Observable<Chatbox> {
-    return this.http.post<Chatbox>(BASE_URL, item)
+    return this.http.post<Chatbox>(BASE_URL, item, httpOptions)
   }
   deleteItem(item: Chatbox | number): Observable<Chatbox> {
     const id = typeof item === 'number' ? item : item.id;
     const url = `${BASE_URL}/${id}`;
 
-    return this.http.delete<Chatbox>(url);
+    return this.http.delete<Chatbox>(url, httpOptions);
   }
 
 }
